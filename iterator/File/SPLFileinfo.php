cf8632289626d961f60ccaa8ee25bf1dce686ecc
GitLigbee <ligbee@outlook.com>
2017-07-22 12:10:21 +0800

    add

--- iterator/File/SPLFileinfo.php
+++ iterator/File/SPLFileinfo.php
@@ -1,5 +1,7 @@
 <?php
-$file = new SplFileInfo('Outeriterator.php');
+$dir = __DIR__;
+$fileRoute = $dir . DIRECTORY_SEPARATOR . 'grammarcheck.php';
+$file = new SplFileInfo($fileRoute);
 echo 'File is create at '.date("Y-m-d H:i:s",$file->getCTime())."\n";
 echo 'File is modified at '.date("Y-m-d H:i:s",$file->getMTime())."\n";
 echo "File size is ".$file->getSize()."\n";
---
+++ iterator/File/m/controller/mc.php
@@ -0,0 +1,2 @@
+<?php
+echo 'mc' . PHP_EOL;
